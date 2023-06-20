fc3eb42f574b27947e5328ee6a777b40564d59f8
[Damanjeet Kaur] <[kaurd56@mymacewan.ca]>
2023-06-19 18:44:11 -0600

    2nd Assignment

---
+++ Assignment2/Assignment_2/Assignment_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//FABIO DA MATA OLIVEIRA (3138693)
+using Assignment_2;
+
+var list1 = new Repository<T>();
+
+Console.WriteLine(list1.Add(new T()));
+Console.WriteLine(list1.Add(new T()));
+Console.WriteLine(list1.Delete(new T()));
+Console.WriteLine(list1.Update(new T(), new T()));
+
+Console.WriteLine(list1.GetAll());
+
+Console.ReadLine();
---
+++ Assignment2/Assignment_2/Assignment_2/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    public class Repository<Data>
+    {
+        public List<Data> Container { get; set; }  
+
+        public Repository() { 
+            Container = new List<Data>();
+        }
+
+        public string Add(Data item)
+        {
+            Container.Add(item);
+
+            return "Add";
+        }
+
+        public string Delete(Data item)
+        {
+            if (Container.Contains(item))
+            {
+                Container.Remove(item);
+            }
+           
+            return "Delete";
+        }
+
+        public string Update(Data newItem, Data oldItem)
+        {
+            for (int i = 0; i < Container.Count; i++)
+            {
+                if (Container[i] != null && Container[i].Equals(oldItem))
+                {
+                    Container[i] = newItem;
+                }
+            }
+           
+            return "Update";
+        }
+
+        public string GetAll()
+        {
+            return "GetAll";
+        }
+
+        public Data CreateNew()
+        {
+            var newD = default(Data);
+            Container.Add(newD);
+            return newD;
+        }
+    }
+}
