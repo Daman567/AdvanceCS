9d206441200acbbc1eaa1ad6b7798f2a56f05e47
[Damanjeet Kaur] <[kaurd56@mymacewan.ca]>
2023-06-21 19:43:28 -0600

    thirdAssignment

---
+++ Assignment3/Core/Interfaces/ICreatureService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ICreatureService
+    {
+        public void InitializeCreatures(int preyCount, int predatorCount);
+       
+
+        public bool IsExtinction();
+        public void Move(int timeout);
+    }
+}
